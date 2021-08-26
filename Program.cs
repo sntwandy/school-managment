d67f88c6147fdc88320a1f5825daf17c4118e254
Wandy Santana <sntwandy@gmail.com>
2021-08-25 22:44:35 -0400

    Creating an array objects and a new method to print the array using while

--- Program.cs
+++ Program.cs
@@ -9,20 +9,36 @@ namespace Core
         static void Main(string[] args)
         {
             var school = new School("Darcy Institute", 2017, SchoolTypes.HighSchool, city: "La Vega", country:"DR");
-            var courseOne = new Course()
+
+            // Create course objects array of 3
+            var courseArray = new Course[3];
+
+            // Print an course array
+            static void PrintCourses(Course[] courseArray)
+            {
+                int counter = 0;
+                while (counter < courseArray.Length)
+                {
+                    Console.WriteLine($"Course name: {courseArray[counter].Name}, Course Id: {courseArray[counter].UniqueId}");
+                    counter++;
+                }
+            }
+
+            courseArray[0] = new Course()
             {
                 Name = "101"
-            };
-            var courseTwo = new Course()
+             };
+            courseArray[1] = new Course()
             {
-                Name = "201"
-            };
-            var courseThird = new Course()
+               Name = "201"
+             };
+            courseArray[2] = new Course()
             {
-                Name = "301"
-            };
+               Name = "301"
+             };
+
             Console.WriteLine(school);
-            Console.WriteLine(courseOne.Name + ", " + courseOne.UniqueId);
+            PrintCourses(courseArray);
         }
     }
 }
