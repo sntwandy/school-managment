b4e3d89f9ceee75e930b1b9fa67ede94b8cbab80
Wandy Santana <sntwandy@gmail.com>
2021-08-25 23:13:13 -0400

    Using differents ways to print and array, for, do while, while and foreach

--- Program.cs
+++ Program.cs
@@ -13,14 +13,35 @@ namespace Core
             // Create course objects array of 3
             var courseArray = new Course[3];
 
-            // Print an course array
+            // Print an course array with differents ways
             static void PrintCourses(Course[] courseArray)
             {
-                int counter = 0;
-                while (counter < courseArray.Length)
+                // Using while
+                /*int counterWhile = 0;
+                while (counterWhile < courseArray.Length)
                 {
-                    Console.WriteLine($"Course name: {courseArray[counter].Name}, Course Id: {courseArray[counter].UniqueId}");
-                    counter++;
+                    Console.WriteLine($"Course name: {courseArray[counterWhile].Name}, Course Id: {courseArray[counterWhile].UniqueId}");
+                    counterWhile++;
+                }*/
+
+                // Using do while
+                /*int counterDo = 0;
+                do
+                {
+                    Console.WriteLine($"Course name: {courseArray[counterDo].Name}, Course Id: {courseArray[counterDo].UniqueId}");
+                    counterDo++;
+                } while (counterDo < courseArray.Length);*/
+
+                // Using for
+               /*for (int i = 0; i < courseArray.Length; i++)
+                {
+                    Console.WriteLine($"Course name: {courseArray[i].Name}, Course Id: {courseArray[i].UniqueId}");
+                };*/
+
+                // Using foreach
+                foreach (Course course in courseArray)
+                {
+                    Console.WriteLine($"Course name: {course.Name}, Course Id: {course.UniqueId}");
                 }
             }
 
